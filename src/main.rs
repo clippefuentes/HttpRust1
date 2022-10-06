77240f1947feae6986c081c256361b48eb1ef1d3
Clyne Fuentes <clippefuentes@gmail.com>
2022-10-05 19:47:34 -0600

    put code into modules

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,12 @@
+
+use server::Server;
+use http::Request;
+use http::Method;
+
+mod server;
+mod http;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Server {
-            addr
-        }
-    }
-
-    fn run(self) {
-        println!("server: {}", self.addr);
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH
 }
