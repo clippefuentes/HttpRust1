6f6341cc18694e08fdcf6a6bf4eae32508f8d1a2
Clyne Fuentes <clippefuentes@gmail.com>
2022-10-13 22:41:42 -0600

    add TryFrom for request strycture

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,5 @@
 use super::method::Method;
+use std::{convert::TryFrom};
 
 pub struct Request {
     path: String,
@@ -6,3 +7,16 @@ pub struct Request {
     method: Method,
 }
 
+impl Request {
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
+        unimplemented!();
+    }
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!();
+    }
+}
